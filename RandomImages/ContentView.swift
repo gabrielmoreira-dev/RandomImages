ded6b3b82063aef56f7f51fdb53259180b042fb4
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-10-10 00:47:29 -0300

    Initial Commit

---
+++ RandomImages/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomImages
+//
+//  Created by Gabriel Moreira on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomImages/RandomImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImagesApp.swift
+//  RandomImages
+//
+//  Created by Gabriel Moreira on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
